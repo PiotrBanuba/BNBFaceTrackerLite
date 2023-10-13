ff996c154e297acdf80890692c402bb02ba35458
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-13 09:51:44 +0300

    1.8.0-10

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-9"
+let bnbPackageVersion: Version = "1.8.0-10"
 
 let package = Package(
     name: "BNBFaceTrackerLite",
@@ -13,9 +13,9 @@ let package = Package(
             name: "BNBFaceTrackerLite",
             targets: [
                 "BNBFaceTrackerLite",
-                "BNBFaceTrackerLiteBNBSdkCore",
-                "BNBFaceTrackerLiteBNBEffectPlayer",
-                "BNBFaceTrackerLiteBNBScripting"
+                "BNBSdkCore",
+                "BNBEffectPlayer",
+                "BNBScripting"
             ]
         ),
     ],
@@ -38,33 +38,6 @@ let package = Package(
             name: "BNBFaceTrackerLite",
             url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-58-g46079e7d74/BNBFaceTrackerLite.zip",
             checksum: "df7ed502989354c369a2532f8061bbd1ceb672ccd3c21537eba0c51e084d0fcc"
-        ),
-        .target(
-            name: "BNBFaceTrackerLiteBNBSdkCore",
-            dependencies: [
-                .product(
-                    name: "BNBSdkCore",
-                    package: "BNBSdkCore"
-                ),
-            ]
-        ),
-        .target(
-            name: "BNBFaceTrackerLiteBNBEffectPlayer",
-            dependencies: [
-                .product(
-                    name: "BNBEffectPlayer",
-                    package: "BNBEffectPlayer"
-                ),
-            ]
-        ),
-        .target(
-            name: "BNBFaceTrackerLiteBNBScripting",
-            dependencies: [
-                .product(
-                    name: "BNBScripting",
-                    package: "BNBScripting"
-                ),
-            ]
         ),
     ]
 )
